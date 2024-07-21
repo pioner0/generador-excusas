c16190f28dc3a8726e11047426d8505d59f393e9
Eduardo Bentancour <42121260+pioner0@users.noreply.github.com>
2024-07-21 18:29:40 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import "./assets/img/rigo-baby.jpg";
+let quien1 = [
+  "El dinosaurio",
+  "Mi tia",
+  "Mi murcielago",
+  "Mi cascarudo",
+  "Mi hermano"
+];
+let que1 = ["se comió", "aplastó", "robó", "rompió", "ensució"];
+let cuando1 = [
+  "antes de ir a trabajar",
+  "justo cuando salia a tomar el omnibus",
+  "mientras merendaba",
+  "cuando me estaba bañando"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function excuseGenerator(quien, que, cuando) {
+  let numQuien = Math.floor(Math.random() * quien.length);
+  let numQue = Math.floor(Math.random() * que.length);
+  let numCuando = Math.floor(Math.random() * cuando.length);
+  return (
+    quien[numQuien] +
+    " " +
+    que[numQue] +
+    " " +
+    "mi tarea" +
+    " " +
+    cuando[numCuando]
+  );
+}
+function onLoad() {
+  var excusa = document.getElementById("excusa");
+  excusa.innerHTML = excuseGenerator(quien1, que1, cuando1);
+}
+window.onload = onLoad;
